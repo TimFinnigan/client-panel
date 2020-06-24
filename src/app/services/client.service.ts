25b885d289511c89ae380c0395aa6c36f754a3b9
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-24 11:58:55 -0400

    created Client model and updated service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Client } from '../models/Client';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ClientService {
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+  clients: Observable<Client>;
+  client: Observable<Client>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.clientsCollection = this.afs.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+  }
+
+  getClients(): Observable<Client[]> {
+    //get clients with the id
+    this.clients = this.clientsCollection.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Client;
+          data.id = action.payload.doc.id;
+          return data;
+        });
+      }));
+    return this.clients;
+  }
 }
