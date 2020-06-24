662be81af9f8ab0e049a4b56409ad525475592a9
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-24 11:40:03 -0400

    add new client service

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ClientService {
+
+  constructor() { }
+}
