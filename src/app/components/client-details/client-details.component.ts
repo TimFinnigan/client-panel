63b8e081d135f7063ad49ba119660375cb0772d6
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-25 16:23:58 -0400

    add deleteClient and onDeleteClick functions

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -49,4 +49,14 @@ export class ClientDetailsComponent implements OnInit {
     this.router.navigate([]);
   }
 
+  onDeleteClick() {
+    if (confirm('Are you sure?')) {
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Client removed', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    }
+  }
+
 }
