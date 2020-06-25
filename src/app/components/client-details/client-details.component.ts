80ad371446843186caa0425057af0272df392439
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-25 10:58:09 -0400

    create getClient method to use in client-details

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+// import { Client } from '../..models/Client';
+import { Client } from 'src/app/models/Client';
 
 @Component({
   selector: 'app-client-details',
   templateUrl: './client-details.component.html',
   styleUrls: ['./client-details.component.css']
 })
-export class ClientDetailsComponent implements OnInit {
 
-  constructor() { }
+export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flash: FlashMessagesService
+  ) { }
 
   ngOnInit(): void {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => { this.client = client; console.log(this.client) });
+
   }
 
 }
