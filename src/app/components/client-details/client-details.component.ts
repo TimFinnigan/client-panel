90aedf3d8dcb27be8bc84d3591dfdbe3fae341e3
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-26 11:13:30 -0400

    update navbar to handle logged in/out scenarios

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
-// import { Client } from '../..models/Client';
 import { Client } from 'src/app/models/Client';
 
 @Component({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +17,9 @@ export class AuthService {
 
   getAuth() {
     return this.afAuth.authState;
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
